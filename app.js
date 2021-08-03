5ff593de1d4d1759d78672c3c21f4538952a219e
Zubair Siddique <zubairsidd20@gmail.com>
2021-08-03 11:36:33 +0530

    basic logic is completed

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const dateInput = document.querySelector("#datePicker");
 const luckyNumber = document.querySelector("#luckyNumber");
 const submitButton = document.querySelector("#submitButton");
 const result = document.querySelector("#result");
+
+const handleClick = () => {
+  let dates = dateInput.value.split("-");
+  let sumOfDate = 0;
+  for (let date of dates) {
+    sumOfDate += parseInt(date);
+  }
+  let isBdLucky = sumOfDate % parseInt(luckyNumber.value);
+  if (isBdLucky === 0) {
+    result.innerHTML = `${dates[2]} is your lucky birthday`;
+  } else {
+    result.innerHTML = `${dates[2]} is not your lucky birthday`;
+  }
+};
+
+submitButton.addEventListener("click", handleClick);
